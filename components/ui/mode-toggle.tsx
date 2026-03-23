8011725303824611d0dc67bacec01713d71d1193
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-23 22:55:28 +0530

    fix radix ui issuse

--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -3,12 +3,7 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-} from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./dropdown-menu";
 
 export function ModeToggle() {
     const { setTheme } = useTheme();
