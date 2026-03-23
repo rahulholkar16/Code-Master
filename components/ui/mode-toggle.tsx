d25c4c6d407aac279ce7d122ccce8522eebc5b29
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-23 17:24:35 +0530

    update ui components

--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
+    DropdownMenuTrigger,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "@radix-ui/react-dropdown-menu";
 
 export function ModeToggle() {
     const { setTheme } = useTheme();
