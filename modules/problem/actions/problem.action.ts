c12dea650106c0df815e331c940131144830d6b5
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-27 20:21:33 +0530

    Add fetch problem by Id

--- modules/problem/actions/problem.action.ts
+++ modules/problem/actions/problem.action.ts
@@ -35,3 +35,35 @@ export const getAllProblem = async () => {
         };
     }
 };
+
+export const getProblemById = async (id: string) => {
+    try {
+        const session = await auth.api.getSession({
+            headers: await headers(),
+        });
+
+        if (!session) return {
+            success: false,
+            message: "Unauthorized",
+        }
+
+        const problem = await db.problem.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        return {
+            success: true,
+            message: "Problem fetched successfully",
+            data: problem,
+        };
+    } catch (error) {
+        console.error("Error fetching problem:", error);
+
+        return {
+            success: false,
+            message: "Something went wrong while fetching problem",
+        };
+    }
+};
