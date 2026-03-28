a465d4c821cb8c14965a245b65c511e0ef56dae9
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-28 20:51:40 +0530

    Add Get difficulty color fn

--- modules/problem/constant.ts
+++ modules/problem/constant.ts
@@ -4,3 +4,16 @@ export const languages = [
     "java",
     "cpp",
 ];
+
+export const getDifficultyColor = (difficulty: string) => {
+    switch (difficulty) {
+        case "Easy":
+            return "bg-green-500/10 text-green-500 border-green-500/20";
+        case "Medium":
+            return "bg-yellow-500/10 text-yellow-500 border-yellow-500/20";
+        case "Hard":
+            return "bg-red-500/10 text-red-500 border-red-500/20";
+        default:
+            return "";
+    }
+};
