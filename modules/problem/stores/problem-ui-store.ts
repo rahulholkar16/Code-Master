651f9671bbbffb8affe7c1bcc48b7a35343e1b11
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-30 20:26:11 +0530

    Add problem ui store

---
+++ modules/problem/stores/problem-ui-store.ts
@@ -0,0 +1,19 @@
+import { Difficulty, ProblemUIStore } from "@/types";
+import { create } from "zustand";
+
+export const useUiProblmStore = create<ProblemUIStore>((set) => ({
+    title: "",
+    difficulty: Difficulty.MEDIUM,
+    tags: [],
+
+    setTitle: (title) => set({ title }),
+    setTag: (tag) => set((state) => ({
+        tags: (state.tags as string[]).includes(tag) ? state.tags : [...state.tags, tag],
+    })),
+    setDifficulty: (difficulty) => set({
+        difficulty
+    }),
+    removeTag: (tag) => set((state) => ({
+        tags: state.tags.filter((t) => t !== tag),
+    })),
+}));
