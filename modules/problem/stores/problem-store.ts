116eb7d96bb06601031bd052463ef90d2c0f91da
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-02 02:00:10 +0530

    Add some new function and selectedProblem state

--- modules/problem/stores/problem-store.ts
+++ modules/problem/stores/problem-store.ts
@@ -1,12 +1,30 @@
 import { ProblmStore } from "@/types";
 import { create } from "zustand";
 
-export const useProblmStore = create<ProblmStore>((set) => ({
+export const useProblmStore = create<ProblmStore>((set, get) => ({
     problems: [],
+    selectedProblem: null,
+
     setProblems: (problems) => set({ problems }),
     setProblem: (problem) => {
         set((state) => ({
             problems: [...(state.problems ?? []), problem]
         }))
-    }
+    },
+
+    setSelectedProblem: (problem) => set({ selectedProblem: problem }),
+    getProblemById: (id) => {
+        return get().problems.find((problem) => problem.id === id);
+    },
+    addProblem: (problem) => {
+        set((state) => {
+            const exists = state.problems.some((p) => p.id === problem.id);
+
+            if (exists) return state;
+
+            return {
+                problems: [...state.problems, problem],
+            };
+        });
+    },
 }));
