8304c3a3c7694d50910eef0b1cc1c16a0560ae8c
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-03 21:51:29 +0530

    Remove Problem Layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { QueryProvider } from "@/components/provider/query-provider";
 import { CircleX } from "lucide-react";
 import { ThemeProvider } from "@/components/provider/theme-provider";
 import AuthLayer from "@/modules/auth/components/auth-layer";
-import ProblemsLayer from "@/modules/problem/components/problems-layer";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -41,7 +40,7 @@ export default function RootLayout({
                 >
                     <QueryProvider>
                         <AuthLayer>
-                            <ProblemsLayer>{children}</ProblemsLayer>
+                            {children}
                         </AuthLayer>
                     </QueryProvider>
                 </ThemeProvider>
