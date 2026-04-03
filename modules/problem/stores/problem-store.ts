37edcdd650ef2d4d6ca726d0d590f36f7e17cf54
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-03 21:54:37 +0530

    update Add Problem Store

--- modules/problem/stores/problem-store.ts
+++ modules/problem/stores/problem-store.ts
@@ -6,21 +6,30 @@ export const useProblmStore = create<ProblmStore>((set, get) => ({
     selectedProblem: null,
 
     setProblems: (problems) => set({ problems }),
+
     setProblem: (problem) => {
         set((state) => ({
-            problems: [...(state.problems ?? []), problem]
-        }))
+            problems: [...(state.problems ?? []), problem],
+        }));
     },
 
     setSelectedProblem: (problem) => set({ selectedProblem: problem }),
+
     getProblemById: (id) => {
         return get().problems.find((problem) => problem.id === id);
     },
+
     addProblem: (problem) => {
         set((state) => {
             const exists = state.problems.some((p) => p.id === problem.id);
 
-            if (exists) return state;
+            if (exists) {
+                return {
+                    problems: state.problems.map((p) =>
+                        p.id === problem.id ? { ...p, ...problem } : p
+                    ),
+                };
+            }
 
             return {
                 problems: [...state.problems, problem],
