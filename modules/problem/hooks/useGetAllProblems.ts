7ff642da3351b5f59e84df8bd2dd00cf55df97f6
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-03 21:53:29 +0530

    Add Use Effect to set data in zustand store

--- modules/problem/hooks/useGetAllProblems.ts
+++ modules/problem/hooks/useGetAllProblems.ts
@@ -1,21 +1,24 @@
 "use client";
 
+import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { useProblmStore } from "@/modules/problem/stores/problem-store";
+import { useProblmStore } from "../stores/problem-store";
 import { getAllProblem } from "../actions/problem.action";
 
 export const useGetAllProblems = () => {
     const setProblems = useProblmStore((state) => state.setProblems);
 
-    return useQuery({
+    const query = useQuery({
         queryKey: ["problems"],
-        queryFn: async () => {
-            const res = await getAllProblem();
-            if (res?.success && res.data) {
-                setProblems(res.data);
-            }
-            return res;
-        },
-        staleTime: 1000 * 60 * 5,
+        queryFn: getAllProblem,
+        staleTime: 1000 * 60 * 5, // 5 min cache
     });
+
+    useEffect(() => {
+        if (query.data?.success && query.data.data) {
+            setProblems(query.data.data);
+        }
+    }, [query.data, setProblems]);
+
+    return query;
 };
