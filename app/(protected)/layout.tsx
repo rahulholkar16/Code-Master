c74078aabf25173bc056d136b8b2294daea984a9
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-11 12:47:45 +0530

    remove proted layer

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,13 +1,12 @@
-import { ProtectedLayer } from "@/modules/auth/components/protected-layer";
 import Navbar from "@/modules/home/components/Navbar";
 import { ChildrenProps } from "@/types";
 
 const ProtectedLayout = ({ children }: ChildrenProps) => {
     return (
-        <ProtectedLayer>
+        <>
             <Navbar />
             {children}
-        </ProtectedLayer>
+        </>
     );
 };
 
