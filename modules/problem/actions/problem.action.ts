b86429423213cf341d38a50ef8feb71e3503e21b
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-13 09:26:16 +0530

    Add Execute code logic

--- modules/problem/actions/problem.action.ts
+++ modules/problem/actions/problem.action.ts
@@ -1,8 +1,12 @@
 "use server";
 
+import { auth } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pollBatchResults, submitBatch } from "@/lib/judge0";
 import { useAuthStore } from "@/modules/auth/store/auth-store";
 import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
+import { getLanguageName } from "../constant";
 
 export const getAllProblem = async () => {
     try {
@@ -95,4 +99,122 @@ export const deleteProblemById = async (id: string) => {
             message: "Something went wrong while Deleteing problem",
         };
     }
+};
+
+export const executeCode = async (source_code: string, language_id: number, stdin: Array<string>, expected_outputs: Array<string>, problem_id: string) => {
+    try {
+        const session = await auth.api.getSession({
+            headers: await headers()
+        });
+
+        if (!session) {
+            return {
+                success: false,
+                message: "Unauthorized",
+            };
+        }
+
+        if (!Array.isArray(stdin) || stdin.length === 0 || !Array.isArray(expected_outputs) || expected_outputs.length !== stdin.length) return {
+            success: false,
+            message: "Invalid testCases",
+        }
+
+        const submissions = stdin.map((input) => ({
+            source_code,
+            language_id,
+            stdin: input,
+            base64_encode: false,
+            wait: false,
+        }));
+
+        const submitResponse = await submitBatch(submissions)
+        const tokens = submitResponse.map((res) => res.token);
+        const results = await pollBatchResults(tokens);
+        let allPassed = true;
+
+        const detailedResults = results.map((result, idx) => {
+            const stdout = result.stdout?.trim();
+            const expected_output = expected_outputs[idx]?.trim();
+            const passed = stdout === expected_output
+            if (!passed) allPassed = false;
+
+            return {
+                testCase: idx+1,
+                passed,
+                stdout,
+                expected: expected_output,
+                stderr: result.stderr,
+                compile_output: result.compile_output,
+                status: result.status.description,
+                memory: result.memory ? `${result.memory} KB` : undefined,
+                time: result.time ? `${result.time} s` : undefined,
+            }
+        });
+
+        const submission = await db.submission.create({
+            data: {
+                userId: session.user.id,
+                problemId: problem_id,
+                sourceCode: source_code,
+                language: getLanguageName(language_id),
+                stdin: stdin.join("\n"),
+                stdout: JSON.stringify(detailedResults.map(r => r.stdout)),
+                stderr: detailedResults.some(r => r.stderr) ? JSON.stringify(detailedResults.map(r => r.stderr)) : null,
+                compileOutput: detailedResults.some(r => r.compile_output) ? JSON.stringify(detailedResults.map(r => r.compile_output)) : null,
+                status: allPassed ? "Accepted" : "Wrong Answer",
+                memory: detailedResults.some(r => r.memory) ? JSON.stringify(detailedResults.map(r => r.memory)) : null,
+                time: detailedResults.some(r => r.time) ? JSON.stringify(detailedResults.map(r => r.time)) : null,
+
+            }
+        });
+
+        if (allPassed) {
+            await db.problemSolved.upsert({
+                where: {
+                    userId_problemId: {userId: session.user.id, problemId: problem_id},
+
+                },
+                update: {},
+                create: {
+                    userId: session.user.id,
+                    problemId: problem_id,
+                }
+            })
+        }
+
+        const testCaseResult = detailedResults.map((result) => ({
+            submissionid: submission.id,
+            testCase: result.testCase,
+            passed: result.passed,
+            stdout: result.stdout,
+            expected: result.expected,
+            stderr: result.stderr,
+            compileOutput: result.compile_output,
+            status: result.status,
+            memory: result.memory,
+            time: result.time
+        }));
+
+        await db.testCaseResult.createMany({ data: testCaseResult });
+        
+        const submissionWithTestCases = await db.submission.findUnique({
+            where: {
+                id: submission.id
+            },
+            include: { testCases: true }
+        });
+
+        return {
+            success: true,
+            submission: submissionWithTestCases
+        }
+    } catch (error) {
+        console.error("Error Deleteing problem:", error);
+
+        return {
+            success: false,
+            message: "Something went wrong.",
+        };
+    }
+    
 };
