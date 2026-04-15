abae2d7fedd0ee066bde510c1aceb60d0f00d9b2
rahulholkar16 <usefulltech1819@gmail.com>
2026-04-15 19:23:59 +0530

    fix type error

--- modules/problem/hooks/useGetAllProblems.ts
+++ modules/problem/hooks/useGetAllProblems.ts
@@ -4,21 +4,22 @@ import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useProblmStore } from "../stores/problem-store";
 import { getAllProblem } from "../actions/problem.action";
+import { Problem } from "@/types";
 
 export const useGetAllProblems = () => {
-    const setProblems = useProblmStore((state) => state.setProblems);
+    const addProblem = useProblmStore((state) => state.addProblem);
 
     const query = useQuery({
         queryKey: ["problems"],
         queryFn: getAllProblem,
-        staleTime: 1000 * 60 * 5, // 5 min cache
+        staleTime: 1000 * 60 * 5,
     });
 
     useEffect(() => {
         if (query.data?.success && query.data.data) {
-            setProblems(query.data.data);
+            query.data.data.forEach((problem: Problem) => addProblem(problem));
         }
-    }, [query.data, setProblems]);
+    }, [query.data, addProblem]);
 
     return query;
 };
