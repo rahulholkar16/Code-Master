ec80185b40768a0e1ba0a321c3ee2f1498b56d9b
rahulholkar16 <usefulltech1819@gmail.com>
2026-05-03 20:02:31 +0530

    Add Get Playlists API

--- app/api/playlist/route.ts
+++ app/api/playlist/route.ts
@@ -1,7 +1,10 @@
 import { auth } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { difficulties } from "@/modules/home/constant";
+import { includes } from "better-auth";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { disconnect, title } from "process";
 
 export async function POST(request: NextRequest) {
     try {
@@ -45,5 +48,59 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({
             success: false, message: "Failed to create PlayList."
         }, { status: 500 });
+    }
+};
+
+export async function GET(request: NextRequest) {
+    try {
+        const session = await auth.api.getSession({
+            headers: await headers()
+        });
+
+        if (!session) {
+            return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
+        }
+
+        const dbUser = await db.findUnique({
+            where: {
+                id: session.user.id
+            }
+        });
+
+        if (!dbUser) return NextResponse.json({
+            success: false, message: "User not found"
+        }, { status: 404 });
+
+        const playlists = await db.playlist.findMany({
+            where: {
+                userId: dbUser.id
+            },
+            includes: {
+                problems: {
+                    includes: {
+                        problem: {
+                            select: {
+                                id: true,
+                                title: true,
+                                difficulty: true,
+
+                            }
+                        }
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+
+        return NextResponse.json({
+            success: false, playlists
+        }, { status: 200 });
+    } catch (error) {
+        console.error("Error fetch playlist:", error);
+        return NextResponse.json({
+            success: false, message: "Failed to fetch playlists"
+        });
     }
 }
