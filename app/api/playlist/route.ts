ddb7a5718d68a5cdd1044181138de0006f3ba166
rahulholkar16 <usefulltech1819@gmail.com>
2026-05-03 19:11:36 +0530

    Add Craete Playlist Post API

---
+++ app/api/playlist/route.ts
@@ -0,0 +1,49 @@
+import { auth } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        const session = await auth.api.getSession({ 
+            headers: await headers()
+        });
+
+        if (!session) {
+            return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
+        }
+
+        const dbUser = await db.findUnique({
+            where: {
+                id: session.user.id
+            }
+        });
+
+        if (!dbUser) return NextResponse.json({
+            success: false, message: "User not found"
+        }, { status: 404 });
+
+        const { name, description } = await request.json();
+
+        if (!name) return NextResponse.json({
+            success: false, message: "Playlist name is required."
+        }, { status: 400 });
+
+        await db.playlist.create({
+            data: {
+                name,
+                description,
+                userId: dbUser.id,
+            }
+        });
+
+        return NextResponse.json({
+            success: true, message: "Playlist craete successfully."
+        }, { status: 200 });
+    } catch (error) {
+        console.error("Error creating playlist::", error);
+        return NextResponse.json({
+            success: false, message: "Failed to create PlayList."
+        }, { status: 500 });
+    }
+}
