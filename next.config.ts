54c2f970bd68cf5ba14eee21e8ee6000f300f587
rahulholkar16 <usefulltech1819@gmail.com>
2026-05-08 23:19:10 +0530

    Add Remot patter

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: "standalone",
   serverExternalPackages: ["@prisma/client", "prisma"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com", // GitHub
+      },
+    ],
+  },
 };
 
 export default nextConfig;
